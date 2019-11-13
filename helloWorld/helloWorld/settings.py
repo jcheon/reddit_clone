0387a0b6651d7a7f33a7f958007f9085e27cca67
Jonathan Cheon <jcheon@mail.csuchico.edu>
2019-11-13 15:28:15 -0800

    changed allowed hosts

--- helloWorld/helloWorld/settings.py
+++ helloWorld/helloWorld/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '551%l6el(4xb1t+218#_$+(@oy0cx4g+3y9idr$2$ewex$1z!r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 LOGIN_REDIRECT_URL = "/"
 
